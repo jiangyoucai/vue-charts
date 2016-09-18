15859e9ff874d41b1aaa319ef9bdefd70a432b32
姜灏 <150haomengting@163.com>
2016-09-18 11:10:56 +0800

    update charts

---
+++ Charts.vue
@@ -0,0 +1,59 @@
+<template lang="html">
+    <div class="charts" id="charts">
+    </div>
+</template>
+
+<script>
+import Echarts from 'Echarts'
+export default {
+    props: ['xData', 'yData', 'zType'],
+    data() {
+        return {}
+    },
+    watch: {
+        xData() {
+            this.createCharts()
+        },
+        yData() {
+            this.createCharts()
+        },
+        zType() {
+            this.createCharts()
+        }
+    },
+    computed: {},
+    ready() {
+        this.createCharts()
+    },
+    attached() {},
+    methods: {
+        createCharts() {
+            const container = document.getElementById('charts')
+            const charts = Echarts.init(container)
+            charts.setOption({
+                title: {},
+                tooltip: {},
+                xAxis: {
+                    data: this.xData
+                },
+                yAxis: {},
+                series: [{
+                    type: this.zType,
+                    data: this.yData
+                }]
+            })
+        }
+    },
+    components: {
+
+    }
+}
+</script>
+
+<style lang="css">
+    .charts{
+        clear: both;
+        width: 100%;
+        min-height: 60vh;
+    }
+</style>
---
+++ Demo.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+    <v-charts :x-data="xData" :y-data="yData" :z-type="zType"></v-charts>
+</template>
+
+<script>
+import Charts from './Charts'
+export default {
+    data() {
+        return {
+            xData: ['js', 'python', 'php', 'lua', 'golang', 'java'],
+            yData: ['100', '85', '63', '71', '92', '78'],
+            zType: 'line'
+        }
+    },
+    computed: {},
+    ready() {},
+    attached() {},
+    methods: {},
+    components: {
+        'v-charts': Charts
+    }
+}
+</script>
+
+<style lang="css">
+</style>
